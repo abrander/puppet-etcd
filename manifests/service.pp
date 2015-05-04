cbf9d180f7cf7a3349b3f26f359de6df5034b31e
Anders Brander <anders@brander.dk>
2015-05-04 08:26:35 +0200

    Removed support for Upstart.

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,25 +1,9 @@
 # == Class etcd::service
 #
 class etcd::service {
-  # Switch service details based on osfamily
-  case $::osfamily {
-    'RedHat' : {
-      $service_file_location = '/etc/init.d/etcd'
-      $service_file_contents = template('etcd/etcd.initd.erb')
-      $service_file_mode     = '0755'
-      $service_provider      = undef
-    }
-    'Debian' : {
-      $service_file_location = '/etc/init/etcd.conf'
-      $service_file_contents = template('etcd/etcd.upstart.erb')
-      $service_file_mode     = '0444'
-      $service_provider      = 'upstart'
-    }
-    default  : {
-      fail("OSFamily ${::osfamily} not supported.")
-    }
-
-  }
+  $service_file_location = '/etc/init.d/etcd'
+  $service_file_contents = template('etcd/etcd.initd.erb')
+  $service_file_mode     = '0755'
 
   # Create the appropriate service file
   if $etcd::manage_service_file {
